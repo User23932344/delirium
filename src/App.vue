5187c8de1f81b7acf0dda7b3c7ae0c5dc34350c8
User23932344 <olegmatusevitch@gmail.com>
2025-03-18 12:29:30 +0300

    feat: first commit

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,31 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+    <div id="app">
+    <div class="header">
+        <HeaderComponent />
+    </div>
+    </div>
 </template>
 
-<style>
+<script>
+import HeaderComponent from './components/Header.vue';
+
+export default{
+    components:{
+        HeaderComponent
+    }
+}
+</script>
+
+
+
+
+<style scoped>
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  height: 100vh; 
+  margin: 0; 
+  background-color: #1C1920; 
 }
 
-nav {
-  padding: 30px;
-}
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
